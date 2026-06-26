12e16e7fc9cd57ca47283d57bf759e6be17757d6
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-26 11:39:28 -0300

    Iniciando WebOS2 e criando lista de apps

---
+++ src/data/osApps.js
@@ -0,0 +1,22 @@
+export const osApps = [
+  {
+    id: "frostLog",
+    label: "Frost Log",
+    icon: "🌧️",
+  },
+  {
+    id: "calculator",
+    label: "Calculator",
+    icon: "🧮",
+  },
+  {
+    id: "projects",
+    label: "Projects",
+    icon: "🗂️",
+  },
+  {
+    id: "settings",
+    label: "Settings",
+    icon: "⚙️",
+  },
+];
